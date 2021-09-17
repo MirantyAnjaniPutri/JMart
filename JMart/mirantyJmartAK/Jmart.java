501c8122e02d428c85f593ea32a56dd5966b7ed2
MirantyAnjaniPutri <miranty.anjani@ui.ac.id>
2021-09-17 22:55:24 +0700

    revisi week 1

--- JMart/mirantyJmartAK/Jmart.java
+++ JMart/mirantyJmartAK/Jmart.java
@@ -7,29 +7,76 @@ package mirantyJmartAK;
  * @author (your name)
  * @version (a version number or a date)
  */
+
+import java.util.Scanner;
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    public int getPromo() {
+        return 0;
+    }
+    
+    public static String getCustomer() {
+        return "oop";
+    }
+    
+    public static float getDiscountPercentage(int before, int after) {
+        float hasil;
+        if (before >= after) {
+            hasil = (before - after) / 100;
+            return hasil;
+        }
+        else {
+            return 0.0f;
+        }
+    }
+    
+    public static int getDiscountedPrice(int price, float discountPercentage) {
+        int discountedPrice;
+        int discountPercent = (int)discountPercentage;
+        if (discountPercent > 100) {
+            discountPercent = 100;
+            return 0;
+        }
+        else {
+            discountedPrice = price - ((discountPercent / 100) * price);
+            return discountedPrice;
+        }
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage) {
+        int discountPercent = (int) discountPercentage;
+        int originalPrice;
+        if (discountPercentage > 0) {
+            originalPrice = (discountedPrice / (100 - discountPercent)) * 100;
+            return originalPrice;
+        }
+        else {
+            return discountedPrice;
+        }
+    }
+    
+    public static float getCommisionMultiplier() {
+        return 0.05f;
+    }
+    
+    public static int getAdjustedPrice(int price) {
+        float price1 = (float) price;
+        float adjustedPrice1 = price1 + (price1 * getCommisionMultiplier());
+        int adjustedPrice = (int) adjustedPrice1;
+        return adjustedPrice;
+    }
+    
+    public static int getAdminFee(int price) {
+        float price1 = (float) price;
+        float adminFee1 = price1 * getCommisionMultiplier();
+        int adminFee = (int) adminFee1;
+        return adminFee;
+    }
+    
     /**
      * Constructor for objects of class Jmart
      */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args) {
+        
     }
 }
