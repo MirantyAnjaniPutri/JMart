fd9230c0dc6431d068dad59a2004ffccf50e7db5
MirantyAnjaniPutri <miranty.anjani@ui.ac.id>
2021-09-18 10:59:11 +0700

    Jmart dihapus

--- JMart/mirantyJmartAK/Jmart.java
+++ JMart/mirantyJmartAK/Jmart.java
@@ -10,69 +10,7 @@ package mirantyJmartAK;
 
 import java.util.Scanner;
 public class Jmart
-{
-    public int getPromo() {
-        return 0;
-    }
-    
-    public static String getCustomer() {
-        return "oop";
-    }
-    
-    public static float getDiscountPercentage(int before, int after) {
-        float hasil;
-        if (before >= after) {
-            hasil = (before - after) / 100;
-            return hasil;
-        }
-        else {
-            return 0.0f;
-        }
-    }
-    
-    public static int getDiscountedPrice(int price, float discountPercentage) {
-        int discountedPrice;
-        int discountPercent = (int)discountPercentage;
-        if (discountPercent > 100) {
-            discountPercent = 100;
-            return 0;
-        }
-        else {
-            discountedPrice = price - ((discountPercent / 100) * price);
-            return discountedPrice;
-        }
-    }
-    
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage) {
-        int discountPercent = (int) discountPercentage;
-        int originalPrice;
-        if (discountPercentage > 0) {
-            originalPrice = (discountedPrice / (100 - discountPercent)) * 100;
-            return originalPrice;
-        }
-        else {
-            return discountedPrice;
-        }
-    }
-    
-    public static float getCommisionMultiplier() {
-        return 0.05f;
-    }
-    
-    public static int getAdjustedPrice(int price) {
-        float price1 = (float) price;
-        float adjustedPrice1 = price1 + (price1 * getCommisionMultiplier());
-        int adjustedPrice = (int) adjustedPrice1;
-        return adjustedPrice;
-    }
-    
-    public static int getAdminFee(int price) {
-        float price1 = (float) price;
-        float adminFee1 = price1 * getCommisionMultiplier();
-        int adminFee = (int) adminFee1;
-        return adminFee;
-    }
-        
+{        
     /**
      * Constructor for objects of class Jmart
      */
